1a34114708944214c8ab6f86c73858f4901dca37
Kevin Armbruster <KevinArmbruster2013@gmail.com>
2022-10-30 10:17:25 +0100

    remove extra method and reformat

--- Lab4/code/rbm.py
+++ Lab4/code/rbm.py
@@ -102,7 +102,7 @@ class RestrictedBoltzmannMachine():
             p_v_h_0 = minibatch
             p_h_v_0, h_0 = self.get_h_given_v(p_v_h_0)
             p_v_h_1, v_1 = self.get_v_given_h(h_0)
-            p_h_v_1, h_1 = self.get_h_given_v(v_1)
+            p_h_v_1, h_1 = self.get_h_given_v(p_v_h_1)  # final update of hidden units should use probs
 
             # [TODO TASK 4.1] update the parameters using function 'update_params'
             self.update_params(p_v_h_0, h_0, p_v_h_1, p_h_v_1)
@@ -111,56 +111,7 @@ class RestrictedBoltzmannMachine():
             # visualize once in a while when visible layer is input images
             if it % self.rf["period"] == 0 and self.is_bottom:
                 viz_rf(weights=self.weight_vh[:, self.rf["ids"]].reshape((self.image_size[0], self.image_size[1], -1)),
-                       it=it, grid=self.rf["grid"])
-
-            # visualize weights as histogram
-            if it % self.rf["period"] == 0:
-                weight_histogram(weights_vh=self.weight_vh, bias_h=self.bias_h, bias_v=self.bias_v, it=it)
-
-            # print progress
-            if it % self.print_period == 0:
-                print("iteration=%7d recon_loss=%4.4f" % (it, self.calc_reconstruction_error(visible_trainset)))
-
-        return
-
-    def cd1_p(self, visible_trainset, n_iterations=10000):
-        
-        """Contrastive Divergence with k=1 full alternating Gibbs sampling
-
-        Args:
-          visible_trainset: training data for this rbm, shape is (size of training set, size of visible layer)
-          n_iterations: number of iterations of learning (each iteration learns a mini-batch)
-        """
-
-        print("learning CD1")
-
-        n_samples = visible_trainset.shape[0]
-
-        for it in range(n_iterations + 1):  # +1 for printing with %-operations
-
-            # [TODO TASK 4.1] run k=1 alternating Gibbs sampling : v_0 -> h_0 -> v_1 -> h_1.
-            # you may need to use the inference functions 'get_h_given_v' and 'get_v_given_h'.
-            # note that inference methods returns both probabilities and activations (samples from probablities)
-            # and you may have to decide when to use what.
-
-            ### Random sampling of minibatch
-            random_rows = np.random.randint(n_samples, size=self.batch_size)
-            minibatch = visible_trainset[random_rows, :]
-
-            ### Gibbs sampling
-            p_v_h_0 = minibatch
-            p_h_v_0, h_0 = self.get_h_given_v(p_v_h_0)
-            p_v_h_1, v_1 = self.get_v_given_h(h_0)
-            p_h_v_1, h_1 = self.get_h_given_v(v_1)
-
-            # [TODO TASK 4.1] update the parameters using function 'update_params'
-            self.update_params(p_v_h_0, h_0, p_v_h_1, h_1)
-
-            ### Visualizations
-            # visualize once in a while when visible layer is input images
-            if it % self.rf["period"] == 0 and self.is_bottom:
-                viz_rf_p(weights=self.weight_vh[:, self.rf["ids"]].reshape((self.image_size[0], self.image_size[1], -1)),
-                       it=it, grid=self.rf["grid"])
+                       it=it, grid=self.rf["grid"], name_add="")
 
             # visualize weights as histogram
             if it % self.rf["period"] == 0:
@@ -218,9 +169,9 @@ class RestrictedBoltzmannMachine():
         weight_vh_gradient = (1 / n_samples * gradient_tmp) + l2_weight_decay
 
         ### calculate weight update with momentum
-        self.delta_bias_v    = self.momentum * self.delta_bias_v    + (1 - self.momentum) * np.sum(v_0 - v_k, axis=0)
+        self.delta_bias_v = self.momentum * self.delta_bias_v + (1 - self.momentum) * np.sum(v_0 - v_k, axis=0)
         self.delta_weight_vh = self.momentum * self.delta_weight_vh + (1 - self.momentum) * weight_vh_gradient
-        self.delta_bias_h    = self.momentum * self.delta_bias_h    + (1 - self.momentum) * np.sum(h_0 - h_k, axis=0)
+        self.delta_bias_h = self.momentum * self.delta_bias_h + (1 - self.momentum) * np.sum(h_0 - h_k, axis=0)
 
         assert self.delta_bias_v.shape == (n_dim_visible,)
         assert self.delta_weight_vh.shape == (n_dim_visible, n_dim_hidden)
@@ -292,15 +243,15 @@ class RestrictedBoltzmannMachine():
 
             support = self.bias_v + hidden_minibatch @ self.weight_vh.T
 
-            labels = softmax(support[:, -self.n_labels:])         
-            support = sigmoid(support[:,:-self.n_labels])
+            labels = softmax(support[:, -self.n_labels:])
+            support = sigmoid(support[:, :-self.n_labels])
 
-            p_v_h = np.concatenate((support, labels), axis = 1)  
+            p_v_h = np.concatenate((support, labels), axis=1)
 
             v = sample_binary(p_v_h[:, :-self.n_labels])
             # labels = sample_categorical(labels)
 
-            v = np.concatenate((v, labels), axis = 1)
+            v = np.concatenate((v, labels), axis=1)
 
         else:
 
@@ -348,7 +299,7 @@ class RestrictedBoltzmannMachine():
         assert p_h_v.shape == (n_samples, self.ndim_hidden)
         assert h.shape == (n_samples, self.ndim_hidden)
         return p_h_v, h
-        #return np.zeros((n_samples, self.ndim_hidden)), np.zeros((n_samples, self.ndim_hidden))
+        # return np.zeros((n_samples, self.ndim_hidden)), np.zeros((n_samples, self.ndim_hidden))
 
     def get_v_given_h_dir(self, hidden_minibatch):
 
@@ -380,10 +331,10 @@ class RestrictedBoltzmannMachine():
             # this case should never be executed : when the RBM is a part of a DBN and is at the top, it will have not have directed connections.
             # Appropriate code here is to raise an error (replace pass below)
 
-            raise Exception("It seems to be an error here") 
+            raise Exception("It seems to be an error here")
 
         else:
-    
+
             # [TODO TASK 4.1] compute probabilities and activations (samples from probabilities) of visible layer (replace the pass and zeros below)
             # (1, self.ndim_visible) - (ndim_visible, ndim_hidden) @ (ndim_hidden, n_samples)
             support = self.bias_v + hidden_minibatch @ self.weight_h_to_v
@@ -395,7 +346,7 @@ class RestrictedBoltzmannMachine():
         assert v.shape == (n_samples, self.ndim_visible)
         return p_v_h, v
 
-        #return np.zeros((n_samples, self.ndim_visible)), np.zeros((n_samples, self.ndim_visible))
+        # return np.zeros((n_samples, self.ndim_visible)), np.zeros((n_samples, self.ndim_visible))
 
     def update_generate_params(self, inps, trgs, preds):
 
